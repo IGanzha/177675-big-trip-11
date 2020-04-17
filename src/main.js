3753cac5babee648c5c9295abc5a156041f9831b
Ilya Ganzha <ilya.ganzha@gmail.com>
2020-04-17 16:46:59 +0300

    4.1. Использование классов. Реализована замена карточки event на event-edit и обратно

--- src/main.js
+++ src/main.js
@@ -1,46 +1,77 @@
-import {createDayTemplate} from './components/day.js';
-import {createCostInfoTemplate} from './components/cost-info.js';
-import {createFilterTemplate} from './components/filter.js';
-import {createMenuTemplate} from './components/menu.js';
-import {createSortTemplate} from './components/sort.js';
-import {createTripDaysSectionTemplate} from './components/trip-days-section.js';
-import {createTripEditTemplate} from './components/trip-edit.js';
-import {createTripInfoTemplate} from './components/trip-info.js';
-import {createEvents} from './mock/trip-event.js';
-import {getGroupedEvents} from './utils.js';
+import DayComponent from './components/day.js';
+import TotalPriceComponent from './components/total-price.js';
+import FilterComponent from './components/filter.js';
+import MenuComponent from './components/menu.js';
+import SortComponent from './components/sort.js';
+import TripDaysSectionComponent from './components/trip-days-section.js';
+import EventComponent from './components/event.js';
+import EventEditComponent from './components/event-edit.js';
+import TripInfoComponent from './components/trip-info.js';
 
+import {getGroupedEvents, render, RenderPosition} from './utils.js';
+import {filterNames} from './mock/filter.js';
+import {createEvents} from './mock/trip-event.js';
 
 const EVENTS_COUNT = 10;
+const events = createEvents(EVENTS_COUNT);
 
-const render = (container, template, place = `beforeend`) => {
-  container.insertAdjacentHTML(place, template);
-};
 
 const tripMainElement = document.querySelector(`.trip-main`);
-render(tripMainElement, createTripInfoTemplate(), `afterbegin`);
+render(tripMainElement, new TripInfoComponent().getElement(), RenderPosition.AFTERBEGIN);
 
 const tripInfoElement = document.querySelector(`.trip-info`);
-render(tripInfoElement, createCostInfoTemplate());
+render(tripInfoElement, new TotalPriceComponent().getElement(), RenderPosition.BEFOREEND);
 
 const tripControlsElement = tripMainElement.querySelector(`.trip-controls`);
 const menuHeaderElement = tripControlsElement.querySelector(`h2`);
-render(menuHeaderElement, createMenuTemplate(), `afterend`);
+render(menuHeaderElement, new MenuComponent().getElement(), RenderPosition.AFTER);
 
-render(tripControlsElement, createFilterTemplate());
+render(tripControlsElement, new FilterComponent(filterNames).getElement(), RenderPosition.BEFOREEND);
 
 const tripEventsSection = document.querySelector(`.trip-events`);
-render(tripEventsSection, createSortTemplate());
+render(tripEventsSection, new SortComponent().getElement(), RenderPosition.AFTERBEGIN);
 
-const events = createEvents(EVENTS_COUNT);
+render(tripEventsSection, new EventEditComponent(events[0]).getElement(), RenderPosition.BEFOREEND);
 
-const groupByDayEvents = getGroupedEvents(events, `dateForGroup`);
+render(tripEventsSection, new TripDaysSectionComponent().getElement(), RenderPosition.BEFOREEND);
 
-render(tripEventsSection, createTripEditTemplate(events[0]));
-render(tripEventsSection, createTripDaysSectionTemplate());
 
-let dayNumber = 0;
+const renderEvent = (eventListElement, event) => {
+  const eventComponent = new EventComponent(event);
+  const onEditButtonClick = () => {
+    eventListElement.replaceChild(eventEditComponent.getElement(), eventComponent.getElement());
+  };
 
-for (const day of Object.keys(groupByDayEvents)) {
-  dayNumber++;
-  render(tripEventsSection, createDayTemplate(day, groupByDayEvents[day], dayNumber));
-}
+  const onEditFormSubmit = (evt) => {
+    evt.preventDefault();
+    eventListElement.replaceChild(eventComponent.getElement(), eventEditComponent.getElement());
+  };
+
+  const editButton = eventComponent.getElement().querySelector(`.event__rollup-btn`);
+  editButton.addEventListener(`click`, onEditButtonClick);
+
+  const eventEditComponent = new EventEditComponent(event);
+  eventEditComponent.getElement().addEventListener(`submit`, onEditFormSubmit);
+  render(eventListElement, eventComponent.getElement(), RenderPosition.BEFOREEND);
+
+};
+
+const renderTripEvents = (eventsArr) => {
+
+  const groupByDayEvents = getGroupedEvents(eventsArr, `dateForGroup`);
+  let dayNumber = 0;
+  for (const day of Object.keys(groupByDayEvents)) {
+    dayNumber++;
+    const dayComponent = new DayComponent(day, dayNumber);
+
+    render(tripEventsSection, dayComponent.getElement(), RenderPosition.BEFOREEND);
+    const dayEventsList = dayComponent.getElement().querySelector(`.trip-events__list`);
+
+    const dayEvents = groupByDayEvents[day];
+    dayEvents.forEach((event) => {
+      renderEvent(dayEventsList, event);
+    });
+  }
+};
+
+renderTripEvents(events);
---
+++ src/mock/filter.js
@@ -0,0 +1,3 @@
+export const filterNames = [
+  `everything`, `future`, `past`
+];
